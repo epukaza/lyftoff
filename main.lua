85d80d9336221cc6411d53d18742323401800505
etisab <ariasabeti@gmail.com>
2016-02-26 16:41:55 -0800

    debounce

--- main.lua
+++ main.lua
@@ -1,1 +1,23 @@
-print("this is main.lua")
+print("starting main.lua")
+
+local pin = 6
+
+function debounce (func)
+    local last = 0
+    local delay = 200000
+
+    return function (...)
+        local now = tmr.now()
+        if now - last < delay then return end
+
+        last = now
+        return func(...)
+    end
+end
+
+function onChange ()
+    print('The pin value has changed to '..gpio.read(pin))
+end
+
+gpio.mode(pin, gpio.INT)
+gpio.trig(pin, 'both', debounce(onChange))
